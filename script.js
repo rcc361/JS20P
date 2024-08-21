4f6b456fbc9a1ddf71b69a6f84446baceda91481
Rachel <54075873+texasrachel@users.noreply.github.com>
2024-08-20 20:19:09 -0500

    styling

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('test');
+let apiQuotes = [];
+
+console.log("Start");
+
+// Get Quotes from API
+async function getQuotes() {
+    const apiURL = 'https://dummyjson.com/quotes';
+    try {
+        const response = await fetch(apiURL);
+        apiQuotes = await response.json();
+        console.log(apiQuotes);
+        } catch (error) {
+        console.log("Error pulling quote");
+    }
+}
+
+
+
+// On Load
+getQuotes();
